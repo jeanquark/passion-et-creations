c196d2c2c42d5c2a5d31fbad7c08b9e3882cde4b
Jean-Marc <jm.kleger@gmail.com>
2021-09-03 01:50:15 +0200

    Handle clicks on media page

--- resources/js/views/admin/medias/index.vue
+++ resources/js/views/admin/medias/index.vue
@@ -29,6 +29,27 @@
 
         <v-row no-gutters>
             tab: {{ tab }}
+            <v-navigation-drawer v-model="showSidebar" app bottom right temporary>
+                <v-list nav dense>
+                    <v-list-item-group active-class="deep-purple--text text--accent-4">
+                        <v-list-item>
+                            <v-list-item-title>Foo</v-list-item-title>
+                        </v-list-item>
+
+                        <v-list-item>
+                            <v-list-item-title>Bar</v-list-item-title>
+                        </v-list-item>
+
+                        <v-list-item>
+                            <v-list-item-title>Fizz</v-list-item-title>
+                        </v-list-item>
+
+                        <v-list-item>
+                            <v-list-item-title>Buzz</v-list-item-title>
+                        </v-list-item>
+                    </v-list-item-group>
+                </v-list>
+            </v-navigation-drawer>
             <v-tabs v-model="tab" align-with-title>
                 <v-tabs-slider color="yellow"></v-tabs-slider>
                 <v-tab href="#tab-1"> Médiathèque </v-tab>
@@ -48,12 +69,21 @@
                                     </template>
                                 </v-breadcrumbs>
                             </v-col>
-                            <v-col cols="6" md="3" lg="2" class="pa-2" v-for="(folder, index) of folders" :key="`folder_${index}`">
-                                <v-img src="/images/icons/folder.png" class="folder" @dblclick="goTo(folder.path, folder.name)"></v-img>
+                        </v-row>
+                        <v-row no-gutters align="end" class="mx-6" style="border: 2px solid green;" @click="handleClick">
+                            <v-col cols="6" md="3" lg="2" class="pa-2" v-for="(folder, index) of folders" :key="`folder_${index}`" >
+                                <v-img src="/images/icons/folder.png" class="link" id="clickableElement" :data-value='{"name": folder.name, "path": folder.path}' data-type="folder" :data-name="folder.name" :data-path="folder.path"></v-img>
                                 <p class="text-center" style="word-break: break-word">{{ folder.name }}</p>
                             </v-col>
-                            <v-col cols="6" md="3" lg="2" class="pa-2" v-for="(file, index) of files" :key="`file_${index}`">
-                                <v-img :src="`/medias${file.path}`" class="image"></v-img>
+                            <v-col
+                                cols="6"
+                                md="3"
+                                lg="2"
+                                class="pa-2"
+                                v-for="(file, index) of files"
+                                :key="`file_${index}`"
+                            >
+                                <v-img :src="`/medias${file.path}`" class="link" id="clickableElement"></v-img>
                                 <p class="text-center" style="word-break: break-word">{{ file.name }}</p>
                             </v-col>
                         </v-row>
@@ -86,18 +116,21 @@ export default {
     },
     data() {
         return {
+            showSidebar: false,
             tab: null,
             tabs: ['Médiathèque', 'Uploader un fichier'],
             files: [],
             folders: [],
             path: '/',
             items: [],
+            clicksCount: 0,
+            clicksTimer: null
         }
     },
     computed: {
         medias() {
             return this.$store.getters['medias/medias']
-        },
+        }
     },
     methods: {
         goTo(folderPath, folderName) {
@@ -112,28 +145,28 @@ export default {
                 this.items.push({
                     name: 'Dossier racine',
                     path: '/',
-                    disabled: true,
+                    disabled: true
                 })
                 this.medias.allDirectories
-                    .map((directory) => directory.split('/'))
-                    .filter((path) => path.length === 1)
-                    .map((folder) => folder[0])
-                    .forEach((folder) =>
+                    .map(directory => directory.split('/'))
+                    .filter(path => path.length === 1)
+                    .map(folder => folder[0])
+                    .forEach(folder =>
                         this.folders.push({
                             name: folder,
                             path: '/' + folder,
-                            type: 'folder',
+                            type: 'folder'
                         })
                     )
                 this.medias.allFiles
-                    .map((filePath) => filePath.split('/'))
-                    .filter((filePath) => filePath.length === 1)
-                    .map((file) => file[0])
-                    .forEach((file) =>
+                    .map(filePath => filePath.split('/'))
+                    .filter(filePath => filePath.length === 1)
+                    .map(file => file[0])
+                    .forEach(file =>
                         this.files.push({
                             name: file,
                             path: '/' + file,
-                            extension: file.substring(file.lastIndexOf('.') + 1),
+                            extension: file.substring(file.lastIndexOf('.') + 1)
                         })
                     )
             } else {
@@ -147,27 +180,27 @@ export default {
                     this.items.push({
                         name: folderName ? folderName : 'Dossier Racine',
                         path: folderName ? array.join('/') : '/',
-                        disabled: index === paths.length - 1 ? true : false,
+                        disabled: index === paths.length - 1 ? true : false
                     })
                 })
                 this.medias.allDirectories
-                    .map((directory) => directory.split('/'))
-                    .filter((a) => a[a.length - 2] === folderName)
-                    .forEach((folder) =>
+                    .map(directory => directory.split('/'))
+                    .filter(a => a[a.length - 2] === folderName)
+                    .forEach(folder =>
                         this.folders.push({
                             name: folder[folder.length - 1],
                             path: '/' + folder.join('/'),
-                            type: 'folder',
+                            type: 'folder'
                         })
                     )
                 this.medias.allFiles
-                    .map((filePath) => filePath.split('/'))
-                    .filter((a) => a[a.length - 2] === folderName)
-                    .forEach((file) =>
+                    .map(filePath => filePath.split('/'))
+                    .filter(a => a[a.length - 2] === folderName)
+                    .forEach(file =>
                         this.files.push({
                             name: file[file.length - 1],
                             path: '/' + file.join('/'),
-                            extension: file.join('/').substring(file.join('/').lastIndexOf('.') + 1),
+                            extension: file.join('/').substring(file.join('/').lastIndexOf('.') + 1)
                         })
                     )
             }
@@ -184,6 +217,39 @@ export default {
             console.log('name: ', name)
             this.goTo(path, name)
         },
+        handleClick(e) {
+            console.log('handleClick e: ', e)
+            console.log('e.target: ', e.target)
+            console.log('e.target.parentNode: ', e.target.parentNode)
+            console.log('e.target.parentNode.id: ', e.target.parentNode.id)
+            console.log('e.target.parentNode[data-value]: ', e.target.parentNode.getAttribute('data-value'))
+            // console.log('abc: ', abc)
+            // console.log('def: ', def)
+            // var target = e.target || e.srcElement
+            // var text = target.textContent || target.innerText
+            // console.log('target: ', target)
+            // console.log('text: ', text)
+            this.clicksCount++
+            if (this.clicksCount === 1) {
+                var self = this
+                this.clicksTimer = setTimeout(() => {
+                    console.log('Click!')
+                    this.clicksCount = 0
+                    // this.selectFolder(folder)
+                    this.showSidebar = !this.showSidebar
+                }, 200)
+            } else {
+                clearTimeout(this.clicksTimer)
+                console.log('Double click!')
+                this.clicksCount = 0
+                // this.navigateToFolder(folder)
+                if (e.target.parentNode.getAttribute('data-type') === 'folder') {
+                    const folderName = e.target.parentNode.getAttribute('data-name')
+                    const folderPath = e.target.parentNode.getAttribute('data-path')
+                    this.goTo(folderPath, folderName)
+                }
+            }
+        },
         formatFileName(file) {
             const index = file.lastIndexOf('/')
             return file.substring(index + 1)
@@ -191,23 +257,23 @@ export default {
         formatRemoveFileExtension(file) {
             const index = file.lastIndexOf('.')
             return file.substring(0, index)
-        },
-    },
+        }
+    }
 }
 </script>
 
 <style scoped>
 .link:hover {
     cursor: pointer;
-    color: red;
+    background: #e9ecef;
 }
 .folder:hover {
     cursor: pointer;
-    background: red;
+    background: #e9ecef;
 }
 .image:hover {
     cursor: pointer;
-    border: 2px solid red;
+    border: 4px solid #e9ecef;
 }
 #breadcrumbs {
     background-color: #e9ecef;
